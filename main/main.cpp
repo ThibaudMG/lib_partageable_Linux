2c37c62adb45c1ce6f2375d5e63d160bf54962e8
ThibaudMG <mthibaudm@hotmail.com>
2021-03-06 20:46:54 +0100

    Get argv

--- main/main.cpp
+++ main/main.cpp
@@ -13,26 +13,29 @@ int main(int argc, char ** argv)
 	
 	void *handle;
 	char *error;
+	
+	std::cout << "argv = '" << argv[1] << "'" << std::endl;
 
 	// Choose component
-	if (argv[1] == "Composant1")
-		handle = dlopen("../lib/libComposant1.so", RTLD_LAZY);
-	else if (argv[1] == "Composant2")
-		handle = dlopen("../lib/libComposant2.so", RTLD_LAZY);
-
+	if (argv[1] == "Composant1") {
+		std::cout << "1" << std::endl;//handle = dlopen("../Composant1/libComposant1.so", RTLD_LAZY);
+	} else if (argv[1] == "Composant2") {
+		std::cout << "2" << std::endl;//handle = dlopen("../Composant2/libComposant2.so", RTLD_LAZY);
+	}
+	/*
 	if (!handle) {
 		fprintf(stderr, "%s\n", dlerror());
 		exit(EXIT_FAILURE);
 	}
 
-	dlerror();    /* Clear any existing error */
+	dlerror();    // Clear any existing error */
 	
 	if (argv[1] == "Composant1") {
-		valeur=composant1(data1,data2);
-		std::cout << getComposant1Version() << std::endl;
-		std::cout << "valeur 1 :" << valeur << std::endl;
+		//valeur=composant1(data1,data2);
+		//std::cout << getComposant1Version() << std::endl;
+		std::cout << "v1" << std::endl;//std::cout << "valeur 1 :" << valeur << std::endl;
 	} else if (argv[1] == "Composant2") {
-		valeur=composant2(data1,data2);
-		std::cout << "valeur 2 :" << valeur << std::endl;
+		//valeur=composant2(data1,data2);
+		std::cout << "v2" << std::endl;//std::cout << "valeur 2 :" << valeur << std::endl;
 	}
 }
